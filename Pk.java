d82e9978581dac2c4bc8b7ba8cbc6ad534a907fa
Praveen <Praveen@gmail.com>
2024-10-28 18:21:32 +0530

    i

--- Pk.java
+++ Pk.java
@@ -1,6 +1,9 @@
 class Pk{
     public static void main(String[] args) {
         System.out.println("b2");
+        System.out.println("b2");
+        System.out.println("b2");
+        System.out.println("b2");
 
     }
 }
